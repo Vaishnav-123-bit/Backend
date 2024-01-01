c20baa70ed772b045f774327f6f761d0c674747e
vaishnav mahdik <1032210456@tcetmumbai.in>
2024-01-01 12:44:08 +0530

    route setup & controller settings

--- 02/src/index.js
+++ 02/src/index.js
@@ -1,16 +1,22 @@
-import mongoose from "mongoose";
-import { DB_name } from "./constants.js";
-import express from "express";
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import dotenv from 'dotenv'
-const app = express();
-
-
+import {app} from './app.js'
 dotenv.config({
-    path:'./env'
+    path: './.env'
 })
 
+
+
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
 
 
 
